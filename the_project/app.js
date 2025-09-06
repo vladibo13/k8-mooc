87be720294ae18748a1ca67d8f0c8719b5854934
vladibo13 <vladibo13@gmail.com>
2025-09-06 14:34:55 +0300

    1.5. The project, step 3

--- the_project/app.js
+++ the_project/app.js
@@ -4,7 +4,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from todo app!");
+  res.send(`
+    <html>
+      <head><title>My Page</title></head>
+      <body>
+        <h1>Hello from Node.js!</h1>
+        <p>This is a raw HTML response.</p>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, () => {
