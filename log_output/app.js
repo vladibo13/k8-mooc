a3711c24631fa052c3b4fabbd4b70b5973b991cf
vladibo13 <vladibo13@gmail.com>
2025-09-11 15:25:46 +0300

    1.7. External access with Ingress

--- log_output/app.js
+++ log_output/app.js
@@ -1,10 +1,28 @@
 const crypto = require('crypto')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
 
-const randomString = crypto.randomUUID(); 
+const randomString = crypto.randomUUID()
 
-console.log("App started. Random string:", randomString);
+console.log("App started. Random string:", randomString)
 
 setInterval(() => {
-  const timestamp = new Date().toISOString();
-  console.log(`${timestamp}: ${randomString}`);
+  const timestamp = new Date().toISOString()
+  console.log(`${timestamp}: ${randomString}`)
 }, 5000);
+
+app.get('/', (req, res) => {
+  res.json('Log Output app running. See <a href="/status">/status</a>.')
+})
+
+app.get('/status', (req, res) => {
+  res.json({
+    timestamp: new Date().toISOString(),
+    randomString
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+});
